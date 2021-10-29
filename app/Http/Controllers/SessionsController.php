cdfe32997b136ad9cc49326ea30861599f625191
moonshadow <1815265375@qq.com>
2021-10-29 02:01:05 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 class SessionsController extends Controller
 {
     public function create(){
@@ -23,5 +23,11 @@ class SessionsController extends Controller
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back()->withInput();
        }
+    }
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
     }
 }
